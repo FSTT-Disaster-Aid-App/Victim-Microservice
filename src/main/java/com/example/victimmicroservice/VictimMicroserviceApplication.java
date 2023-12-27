efba514358b0be54c6e5467c0a30932e577fa3ae
R1A2N <rania.elmagouri@etu.uae.ac.ma>
2023-12-27 10:04:56 +0100

    first commit

---
+++ src/main/java/com/example/victimmicroservice/VictimMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.victimmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictimMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VictimMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/victimmicroservice/VictimMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.victimmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictimMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
